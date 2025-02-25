8373a9587bfaed5c7139188e601accdff3b4814b
Rachel Case <rmc3374@gmail.com>
2025-02-25 11:50:04 -0500

    lecture

--- Lectures/Lecture_2_18.py
+++ Lectures/Lecture_2_18.py
@@ -27,5 +27,8 @@ state_code = input('Pls enter your two digit state code: ')
 if (state_code == 'CA'):
     # print tax subtotal
     print(f'Your final tax bill is ${calculate_tax(TAX_RATE)}')
+elif (state_code == 'NY'):
+    # print tax subtotal
+    print('Your final tax bill is ${calculate_tax90.109)}')
 else:
     print(f'Your final tax bill is ${calculate_tax(0.10)}')
--- Lectures/Lecture_2_25.py
+++ Lectures/Lecture_2_25.py
@@ -1,1 +1,4 @@
-#If/else statements
+# If/else statements
+# Lecture 2_18 has single threaded if/else
+
+# FUN FACT: Plants revolve around sun perfectly in line - sun moves
