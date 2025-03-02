a620e52a8fc70a6f5fe80da4e713f967518ffea3
Rachel Case <rmc3374@gmail.com>
2025-03-01 20:30:32 -0500

    Module3Notes

--- Module 3/IterativeStatementsMicroLectureCoding.py
+++ Module 3/IterativeStatementsMicroLectureCoding.py
@@ -6,9 +6,15 @@ def checkInput(input):
     if (int(response) > 10 or int(response) < 1): 
         return f'{input} is out of bounds. Please try again.'
     # happy path
-    return f'The number is (input)'
+    return f'The number is {input}'
 
 # Iterative Statements
-response = input("Please enter a number between 1 and 10: ")
-while (response != -1): 
-    print(checkInput(repsonse))
+# response = input("Please enter a number between 1 and 10: ")
+
+# Do not put Boolean expressions here inside While loop - Line 15
+# while (response != -1): 
+
+# Best practice for looping in Python
+while (True): 
+    response = input("Please enter a number between 1 and 10: ")
+    print(checkInput(response))
