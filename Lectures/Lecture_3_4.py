ad2d3912c133de1e2f7a384ab2adcfc69db88fc4
Rachel Case <rmc3374@gmail.com>
2025-03-04 12:16:44 -0500

    Lecture

---
+++ Lectures/Lecture_3_4.py
@@ -0,0 +1,16 @@
+# 4 specific situations to loop data:
+    # Filtering (i.e. looking for only even or odd numbers) - built into most programming languages
+    # Sort - bubble sorts, fifo sorts, first in/first out, etc.
+    # Serching - wanting to find something
+    # exceptions
+# loops inherantly not performant (not efficient)
+# dual-while loop executes code before code asks if it should continue
+    # some loops execute without having to make decision
+# loop control variable = sentinel variable/sentinel loop variable
+    # variable happens before loop entered, is tested in loop, body of loop alters value
+# definite loops = happens for predetermined # of times (never used)
+# counter-controlled/counted loop - counts # of loops
+# indefinite loops - executes forever if user doesn't have control
+# for statement/for loop - definite loop
+# do-while/until: do does work before while executes
+# GIGO: garbage in, garbage out - 
