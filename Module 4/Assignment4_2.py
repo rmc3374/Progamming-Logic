b616bb887ab08b0dfedd34ac045e5bf6866afa95
Rachel Case <rmc3374@gmail.com>
2025-03-19 14:40:57 -0400

    productivity score

--- Module 4/Assignment4_2.py
+++ Module 4/Assignment4_2.py
@@ -17,14 +17,16 @@ transactionValue = float(input("Please enter the transaction dollar value: "))
 productivityScore = (transactionValue/transactionAmount) / shifts
 
 # calculate bonus
-if productivityScore <= 30: 
-    employeeBonus = 50.00
-elif 31 <= productivityScore <= 69: 
-    employeeBonus = 75.00
-elif 70 <= productivityScore <= 199: 
-    employeeBonus = 100.00
-elif productivityScore >= 200: 
-    employeeBonus = 200.00
+if productivityScore > 30:
+    if 31 <= productivityScore <= 69: 
+        employeeBonus = 75.00
+    if 70 <= productivityScore <= 199:
+        employeeBonus = 100
+    if productivityScore >= 200: 
+        employeeBonus = 200.00
+else: 
+    employeeBonus = 50.00 
+
 
 # print employeeName
 print(f"Employee Name: {employeeName}")
