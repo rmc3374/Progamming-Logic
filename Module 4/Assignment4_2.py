86475b5fc1ea51426c5cfd365f3d2f353c1e138c
Rachel Case <rmc3374@gmail.com>
2025-03-19 14:19:16 -0400

    productivity bonus

---
+++ Module 4/Assignment4_2.py
@@ -0,0 +1,31 @@
+# input variables
+# employee name
+employeeName = input("Please enter employee name: ")
+
+# number of shifts
+shifts = int(input("Please enter number of shifts: "))
+
+# number of transactions
+transactionAmount = int(input("Please enter the number of transactions: "))
+
+# transaction dollar value
+transactionValue = int(input("Please enter the transaction dollar value: "))
+
+# calculate productivity score
+# productivity score = (employee transaction dollar value / number of transactions) / number of shifts worked
+
+productivityScore = (transactionValue/transactionAmount) / shifts
+
+# calculate bonus
+if productivityScore <= 30: 
+    employeeBonus = 50.00
+elif 31 <= productivityScore <= 69: 
+    employeeBonus = 75.00
+elif 70 <= productivityScore <= 199: 
+    employeeBonus = 100.00
+elif productivityScore >= 200: 
+    employeeBonus = 200.00
+
+# print employeeName
+print(f"Employee Name: {employeeName}")
+print(f"Employee Bonus: $ {employeeBonus}")
