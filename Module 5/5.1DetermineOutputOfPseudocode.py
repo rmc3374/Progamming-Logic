215703d65a85ef53607b9acd20e9475adec450c6
Rachel Case <rmc3374@gmail.com>
2025-03-30 14:28:11 -0400

    output of pseudocode

---
+++ Module 5/5.1DetermineOutputOfPseudocode.py
@@ -0,0 +1,57 @@
+# pseudocode a
+a = 1
+b = 2
+c = 5
+while a < c: 
+    a = a + 1
+    b = b + c
+print(a, b, c)
+
+# pseudocode b
+d = 4
+e = 6
+f = 7
+while d > f: 
+    d = d + 1
+    e = e - 1
+print(d, e, f)
+
+# pseudocode c
+g = 4
+h = 6
+while g < h: 
+    g = g + 1
+print(g, h)
+
+# pseudocode d
+j = 2
+k = 5
+n = 9
+while j< k: 
+    m = 6
+    while m < n: 
+        print("Goodbye")
+        m = m + 1
+    j = j + 1
+
+# pseudocode e
+j = 2
+k = 5
+m = 6
+n = 9
+while j< k: 
+    while m < n: 
+        print("Hello")
+        m = m + 1
+    j = j + 1
+
+# pseudocode f
+p = 2
+q = 4
+while p < q: 
+    print("Adios")
+    r = 1
+    while r < q: 
+        print("Adios")
+        r = r + 1
+    p = p + 1
